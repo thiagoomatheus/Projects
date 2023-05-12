644f054b020f2cb8b9a67a7d492099769040364a
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-05-12 13:32:31 -0300

    feat(script): make a ip validation

--- ip-address-tracker-master/script/script.js
+++ ip-address-tracker-master/script/script.js
@@ -1,3 +1,5 @@
+/* Mapa*/
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,3 +8,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
+
+/* Fim Mapa*/
+
+let inputIp = document.querySelector("#ip")
+
+function validateIp(input) {
+    let ipString = input.split('.');
+    console.log(ipString);
+    let ip = ipString.map(Number);
+    console.log(ip);
+    if (ip.length != 4 || ip[0] < 0 || ip[0] > 255 || ip[1] < 0 || ip[1] > 255 || ip[2] < 0 || ip[2] > 255 || ip[3] < 0 || ip[3] > 255) {
+        return false
+    }
+    else {
+        return true
+    }
+}
